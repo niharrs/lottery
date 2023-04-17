427cde5c7675574ab64d467fed011a22b0f443e3
Niharika Singh <niharika.3297@gmail.com>
2023-04-17 18:13:42 +0200

    Add gas reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -26,11 +26,18 @@ module.exports = {
             accounts: [PRIVATE_KEY],
         },
     },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
     solidity: "0.8.18",
     namedAccounts: {
         deployer: {
             default: 0,
-            1:0
+            1: 0,
         },
         player: {
             default: 1,
@@ -38,8 +45,8 @@ module.exports = {
     },
     etherscan: {
         apiKey: {
-            sepolia: ETHERSCAN_API_KEY
+            sepolia: ETHERSCAN_API_KEY,
         },
-        customChains:[]
-    }
+        customChains: [],
+    },
 }
