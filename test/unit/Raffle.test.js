79677dc83f1dc1e44f8aee74e712831d72ad0dff
Niharika Singh <niharika.3297@gmail.com>
2023-04-18 14:25:48 +0200

    Add unit tests for performUpkeep function

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -76,4 +76,33 @@ const { assert, expect } = require("chai")
                   assert.equal(upkeepNeeded, false)
               })
           })
+
+          describe("performUpkeep function", async function () {
+              it("will run only if checkupkeep is true", async function () {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                  await network.provider.send("evm_mine", [])
+                  const tx = await raffle.performUpkeep([])
+                  assert(tx)
+              })
+
+              it("will not run if checkupkeep is false", async function () {
+                  await expect(raffle.performUpkeep([])).to.be.revertedWith(
+                      "Raffle__UpkeepNotNeeded"
+                  )
+              })
+
+              it("updates the raffle state, emits an event, and calls the vrf coordinator", async function () {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                  await network.provider.send("evm_mine", [])
+                  const txResponse = await raffle.performUpkeep([])
+                  const txReceipt = await txResponse.wait(1)
+                  const requestId = txReceipt.events[1].args.requestId
+                  const raffleState = await raffle.getRaffleState()
+
+                  assert(requestId.toNumber() > 0)
+                  assert.equal(raffleState.toString(), 1)
+              })
+          })
       })
